b2aacdb0d9efed1b5b94ced61b770b41dfb158c3
hello3world <e.pavlovich29@gmail.com>
2025-10-01 23:00:54 +0300

    feat: go on diving

---
+++ playwright/playwrightBasics.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page
+
+def test_playwrightBasics(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rahulshettyacademy.com")
+
+def test_playwrightShortCut(page:Page):
+    page.goto("https://rahulshettyacademy.com")
